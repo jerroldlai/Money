3ab90e3a424db9ae95b8063c79cc0e98fdade3c5
jerrold <jerroldlai@gmail.com>
2016-04-17 20:17:30 +0800

    4/17 re-upload

---
+++ Money/Controllers/MoneyController.cs
@@ -0,0 +1,71 @@
+﻿using Money.Views.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Money.Controllers
+{
+    public class MoneyController : Controller
+    {
+        
+          
+        // GET: Money
+        public ActionResult Money()
+        {
+            List<SelectListItem> listItems = new List<SelectListItem>();
+        listItems.Add(new SelectListItem
+            {
+                Text = "請選擇",
+                Value = "請選擇"
+            });
+
+            listItems.Add(new SelectListItem
+            {
+                Text = "收入",
+                Value = "收入"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "支出",
+                Value = "支出",
+            });
+            ViewBag.CategroyItems = listItems;
+            return View();
+            
+        }
+        
+        public ActionResult List(MoneyViewModel pagedata)
+        {
+            var source = new List<MoneyViewModel>();
+            if (pagedata.category!= null || 
+                pagedata.money != null ||
+                pagedata.description != null ||
+                pagedata.date != null)
+            {
+                source.Add(new MoneyViewModel
+                {
+                    no = source.Count,
+                    category =pagedata.category,
+                    money =pagedata.money,
+                    date =pagedata.date,
+                    description =pagedata.description,
+                });
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                source.Add(new MoneyViewModel
+                {
+                    no = i,
+                    category = "支出",
+                    money = System.Convert.ToString(123 * i),
+                    date = DateTime.Now.AddDays(i+1).ToString("yyyy-MM-dd"),
+                    description = "",
+                });
+            }
+            return View(source);
+        }
+    }
+}
---
+++ Money/Views/ViewModel/MoneyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Money.Views.ViewModel
+{
+    public class MoneyViewModel
+    {
+       public string category { get; set; }
+        public int no {get; set; }
+        public string money { get; set; }
+       public string date { get; set; }
+       public string description { get; set; }
+       
+      
+
+       
+      
+    }
+}
